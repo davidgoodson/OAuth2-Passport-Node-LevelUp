b49cff72c4700ba1bdc8d44d71b8ebf72971e37d
David Buyinza <davidbuyinza256@gmail.com>
2020-04-15 09:02:24 +0300

    Add Password To Local Sign Up

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 "use strict";
+const bcrypt = require("bcrypt");
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     "User",
@@ -7,8 +8,15 @@ module.exports = (sequelize, DataTypes) => {
       firstName: DataTypes.STRING,
       otherName: DataTypes.STRING,
       userLevel: DataTypes.INTEGER,
+      password: DataTypes.STRING,
     },
-    {}
+    {
+      setterMethods: {
+        password(value) {
+          this.setDataValue("password", bcrypt.hashSync(value, 10));
+        },
+      },
+    }
   );
   User.associate = function (models) {
     // associations can be defined here
